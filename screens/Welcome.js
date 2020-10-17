290fad3a4034220d129e7256c41c6e7c16193129
mohammad-abuhummos <mohammad.abuhummoscs@gmail.com>
2020-10-18 01:02:29 +0300

    fix welcome screen  layout

--- screens/Welcome.js
+++ screens/Welcome.js
@@ -2,7 +2,9 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import AppInput from "../components/AppInput";
-
+import AppButton from "../components/AppButton";
+import { Button } from "react-native-elements";
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 export default function Welcome() {
   return (
     <View style={styles.container}>
@@ -11,9 +13,42 @@ export default function Welcome() {
         start={{ x: 0.9, y: 0.0 }}
         style={styles.gradient}
       >
+          
         <Text>Event jo app</Text>
-        <View style={{ paddingHorizontal: 40, width: "100%" }}>
-          <AppInput placeholder="Welcome" />
+        <View
+          style={{ paddingHorizontal: 40, paddingVertical: 20, width: "100%" }}
+        >
+          <AppInput placeholder="USERNAME" />
+        </View>
+        <View
+          style={{ paddingHorizontal: 40, paddingVertical: 20, width: "100%" }}
+        >
+          <AppInput placeholder="USERNAME" />
+        </View>
+        <View
+          style={{ paddingHorizontal: 40, paddingVertical: 20, width: "100%" }}
+        >
+          <AppButton
+            Style={{ backgroundColor: "#fff" }}
+            titleStyle={{ color: "#719CCE" }}
+          />
+        </View>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "center",
+            alignContent: "center",
+          }}
+        >
+          <Text style={{ alignSelf: "center", color: "#fff" }}>
+            Don’t have an account ?
+          </Text>
+          <Button
+            titleStyle={{ color: "#fff" }}
+            buttonStyle={{ paddingLeft: 5 }}
+            title="Sign Up"
+            type="clear"
+          />
         </View>
       </LinearGradient>
     </View>
@@ -31,6 +66,7 @@ const styles = StyleSheet.create({
     flex: 1,
     width: "100%",
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "flex-end",
+    paddingBottom: 150,
   },
 });
