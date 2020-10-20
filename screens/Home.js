def0c5105afc3800319be495e2a5f28e5b7af318
mohammad natour <mohammad.nator@yahoo.com>
2020-10-21 00:52:09 +0300

    fix home page header

--- screens/Home.js
+++ screens/Home.js
@@ -1,28 +1,38 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View, TextInput } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
-import { Avatar, colors, Input,Icon } from 'react-native-elements';
+import { Avatar, Button } from 'react-native-elements';
 import { Colors } from "react-native/Libraries/NewAppScreen";
-
+import { FontAwesome, MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
 export default function SignUp() {
-
     return (
         <View style={styles.container}>
             <LinearGradient colors={["#6AF1C5", "#3D7BF7"]} start={{ x: 0.9, y: 0.0 }} style={styles.gradient}>
-                <View style={styles.iconsHeader}>
-                </View>
-                <View style={styles.profileTitle}>
-                    <View style={styles.profilePic}>
-                        <Avatar size={70} rounded source={{ uri: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg', }} />
+                <View style={styles.containerProfile}>
+                    <View style={styles.iconsHeader}>
+                        <View>
+                            <MaterialCommunityIcons name="menu-open" size={50} color="white" style={styles.menuIcon} />
+                        </View>
+                        <View>
+                            <FontAwesome name="bell" size={24} color="white" style={styles.bellIcon} />
+                        </View>
                     </View>
-                    <View><Text style={styles.profileName} >Hi  , Evana</Text></View>
-                    <View><Text style={styles.profileText} >Another txt</Text></View>
-                    <View><Text style={styles.profileFeatured} >فعاليات مميزة</Text></View>
-                    <View><Text style={styles.profileAllEvent} >مشاهدة الكل</Text></View>
-                </View>
-                <View style={styles.searchSection}>
-                    <TextInput style={styles.searchInput} placeholder="بحث"  onChangeText={value => this.setState({ comment: value })}
-                    />
+                    <View style={styles.profileTitle}>
+                        <View style={styles.profilePic}>
+                            <Avatar size={70} rounded source={{ uri: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg', }} style={styles.avatarStyle} />
+                        </View>
+                        <View style={styles.containerName}><Text style={styles.profileName} >Hi  , Evana</Text>
+                            <Text style={styles.profileText} >Another txt</Text></View>
+                    </View>
+                    <View style={styles.searchSection}>
+                        <TextInput style={styles.searchInput} placeholder="بحث" onChangeText={value => this.setState({ comment: value })}
+                        />
+                        <View style={styles.searchIcon}>
+                            <LinearGradient colors={["#6AF1C5", "#3D7BF7"]} start={{ x: 0.9, y: 0.0 }} style={{ borderRadius: 15 }}>
+                                <MaterialIcons name="search" size={24} color="white" />
+                            </LinearGradient>
+                        </View>
+                    </View>
                 </View>
             </LinearGradient>
         </View>
@@ -33,81 +43,90 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         width: "100%",
-
     },
     gradient: {
         width: "100%",
-        height: 300,
+        minHeight: 300,
         borderBottomLeftRadius: 50,
         borderBottomRightRadius: 50,
     },
     profilePic: {
-        top: 80,
-        marginLeft: 40,
         backgroundColor: "white",
         width: 80,
         height: 80,
         borderRadius: 50,
-        alignItems: "center",
         justifyContent: "center",
+        alignItems: 'center',
+    },
+    containerProfile: {
+        flexDirection: 'column',
     },
     profileTitle: {
-
+        flexDirection: 'row',
+        width: '100%',
+        paddingHorizontal: 50,
+    },
+    containerName: {
+        paddingLeft: 10,
+        paddingTop: 10,
     },
     profileName: {
-        marginLeft: 140,
         fontSize: 25,
         fontWeight: "bold",
         color: 'white',
-        top: 15,
+
+    },
+    avatarStyle: {
+        width: 70,
+        height: 70,
+
     },
     profileSearch: {
-
+        flexDirection: 'row',
     },
     profileText: {
-        marginLeft: 140,
-        fontSize: 15,
-        color: 'white',
-        top: 15,
-    },
-    profileAllEvent: {
-        marginRight: 30,
-        fontSize: 15,
-        color: 'white',
-        top: 75,
 
-    },
-    profileFeatured: {
-        marginRight: 270,
-        fontSize: 17,
+        fontSize: 15,
         color: 'white',
-        top: 95,
-        fontWeight: "bold",
     },
+
     iconsHeader: {
-        borderLeftWidth: 4,
-        borderRightWidth: 4,
-        height: 10
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: '100%',
+        alignItems: 'center',
+        padding: 15,
     },
     searchSection: {
-        flex: 1,
         flexDirection: 'row',
-        width: "80%",
-        marginLeft:50,
+        alignSelf: 'center',
+        marginHorizontal: '10%',
+        marginTop: 20,
+
     },
     searchInput: {
-        paddingRight: 30,
+        paddingRight: 45,
         height: 37,
         borderColor: 'white',
         borderWidth: 1,
         backgroundColor: 'white',
-        borderRadius:20,
-        shadowColor:'gray',
-        flex: 1,
+        borderRadius: 20,
+        width: '100%'
+    },
+    searchIcon: {
+        position: 'absolute',
+        backgroundColor: 'blue',
+        borderRadius: 20,
+        color: 'white',
+        marginTop: 6,
+        right: 10,
+    },
+    bellIcon: {
+    },
+    menuIcon: {
 
     },
 
-  
-   
+
 
 });
