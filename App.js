39fc654a64392302d9fda07ed3de4fee641eca1f
mohammad-abuhummos <mohammad.abuhummoscs@gmail.com>
2020-11-06 23:44:36 +0200

    create app Drawer

--- App.js
+++ App.js
@@ -1,12 +1,50 @@
 import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
+import { StyleSheet, Text, View, Image } from "react-native";
+import { NavigationContainer, DrawerItems } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import {
+  createDrawerNavigator,
+  DrawerItemList,
+  DrawerItem,
+} from "@react-navigation/drawer";
 import SingIn from "./screens/Auth/SignIn";
 import SignUp from "./screens/Auth/SignUp";
 import Home from "./screens/Home";
+
+const Drawer = createDrawerNavigator();
+function CustomDrawerContentComponent(props) {
+  return (
+    <View style={{ paddingVertical: 60 }}>
+      <View style={{ padding: 20 }}>
+        <Image
+          source={require("./assets/card-img-1.png")}
+          style={{ width: 100, height: 100, borderRadius: 50 }}
+        />
+        <Text style={{ fontSize: 20, fontWeight: "bold", paddingLeft: 15 }}>
+          kdcnjdn
+        </Text>
+        <Text style={{ paddingLeft: 15 }}>Bela@gmail.com</Text>
+      </View>
+
+      <DrawerItemList {...props} />
+      <DrawerItem label="Help" onPress={() => alert("Link to help")} />
+    </View>
+  );
+}
+
+function AppDrawer() {
+  return (
+    <Drawer.Navigator
+      initialRouteName="Home"
+      drawerContent={(props) => <CustomDrawerContentComponent {...props} />}
+    >
+      <Drawer.Screen name="Home" component={Home} />
+      <Drawer.Screen name="SingIn" component={SingIn} />
+    </Drawer.Navigator>
+  );
+}
 
 export default function App() {
   const Stack = createStackNavigator();
@@ -20,7 +58,7 @@ export default function App() {
       >
         <Stack.Screen name="SingIn" component={SingIn} />
         <Stack.Screen name="SignUp" component={SignUp} />
-        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Home" component={AppDrawer} />
       </Stack.Navigator>
     </NavigationContainer>
   );
